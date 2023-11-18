266b200ca0251ae24bd1cd335d5c4bcd27287782
matthew-buglass <buglassmatthew@gmail.com>
2023-11-17 18:24:50 -0600

    finished the cycle finding

--- dice.py
+++ dice.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-from graphs import Edge, Vertex, WeightedVertex
+from graphs import Edge, Vertex, WeightedVertex, UndirectedPath
 
 
 class Die:
     cycles: list[list[WeightedVertex]]
     edges: list[Edge]
-    vertex_weights: [WeightedVertex]
+    verts: [WeightedVertex]
     sides: int
 
     def __init__(self, num_faces: int, adjacent_faces: [tuple[int, int]],
-                 faces_on_vertices: list[int], opposing_faces: list[tuple[int, int]]):
+                 num_faces_on_vertices: list[int], opposing_faces: list[tuple[int, int]]):
         """
         An abstract undirected graph representation of a die. Vertices in the graph represent faces on a die, and the vertex
         weights are the face values of the die. A simple cycle of a given number of vertices represents the point, or
@@ -22,16 +22,16 @@ class Die:
         :param num_faces: the number of faces on the die. this will be the number of vertices in the graph.
         :param adjacent_faces: an edge list of faces of the die that are adjacent to each other. A list of tuples
             containing the vertex indices of the start and end vertices of an edge.
-        :param faces_on_vertices: simple cycles in the graph that represent the meeting of a certain number of faces. In
+        :param num_faces_on_vertices: simple cycles in the graph that represent the meeting of a certain number of faces. In
             perfectly fair dice (D4, D6, D8, D12, D20) this will always be the same number of faces (3, 3, 4, 3, 5) within
             the die. For unfair dice (D10, D30, etc.) this may differ in the die (3 and 5, 3 and 5), which is why this
             is a list.
         """
-        self.vertex_weights = np.array([WeightedVertex(i, 0) for i in range(num_faces)])
-        self.edges = adjacent_faces
+        self.verts = np.array([WeightedVertex(i, 0) for i in range(num_faces)], dtype=WeightedVertex)
+        self.edges = [Edge(self.verts[e[0]-1], self.verts[e[1]-1])for e in adjacent_faces]
 
         self.cycles = []
-        for cyc_len in faces_on_vertices:
+        for cyc_len in num_faces_on_vertices:
             self.cycles.extend(self.__find_simple_cycles__(cyc_len))
 
     def __find_simple_cycles__(self, cycle_len) -> list[list[WeightedVertex]]:
@@ -41,21 +41,41 @@ class Die:
         :param cycle_len: The number of unique vertices in the cycles. The first vertex counts as the first and last.
         :return: A list of unique simple cycles in the graph. The closing edge goes from the last to the first vertex
         """
-        def cycle_helper(edge_dict: dict[int: list[Edge]], curr_path: list[WeightedVertex], cycle_len: int, cycles: list[WeightedVertex]):
+        def cycle_helper(edge_dict: dict[int: list[Edge]], curr_path: list[WeightedVertex], cycle_len: int, cycles: list[UndirectedPath]):
             if len(curr_path) == cycle_len:
-                if Edge(curr_path[0], curr_path[-1]) in edge_dict[curr_path[-1]]:
-                    cycles.append(curr_path)
+                # if there's an edge from the last to the first vertex add the path to the collected cycles
+                try:
+                    last_edge = edge_dict[curr_path[-1]][curr_path[0]]
+                    if not curr_path in cycles:
+                        cycles.append(curr_path)
+                except KeyError:
+                    pass
             else:
                 start_vert = curr_path[-1]
-                edges_to_try = [e for e in edge_dict[start_vert] if e.follow(start_vert) not in curr_path]
+                edges_to_try = [e for e in edge_dict[start_vert].values() if e.follow(start_vert) not in curr_path]
                 for edge in edges_to_try:
                     new_path = curr_path.copy()
                     new_path.append(edge.follow(curr_path[-1]))
                     cycle_helper(edge_dict, new_path, cycle_len, cycles)
 
+        cycles = []
+        edge_dict = {}
+        for edge in self.edges:
+            try:
+                edge_dict[edge.src][edge.dst] = edge
+            except KeyError:
+                edge_dict[edge.src] = {}
+                edge_dict[edge.src][edge.dst] = edge
 
+            try:
+                edge_dict[edge.dst][edge.src] = edge
+            except KeyError:
+                edge_dict[edge.dst] = {}
+                edge_dict[edge.dst][edge.src] = edge
 
-
+        for v in self.verts:
+            cycle_helper(edge_dict, UndirectedPath([v]), cycle_len, cycles)
+        return cycles
 
     def __get_vertex_weights__(self) -> list[tuple[list[int], float]]:
         """
@@ -64,7 +84,7 @@ class Die:
         """
         vert_weights = []
         for cycle in self.cycles:
-            weight = sum([self.vertex_weights[w] for w in cycle]) / len(cycle)
+            weight = sum([self.verts[w] for w in cycle]) / len(cycle)
             vert_weights.append((cycle, weight))
         return vert_weights
 
@@ -75,4 +95,10 @@ class Die:
 
 
 if __name__ == '__main__':
+    d4 = Die(
+        num_faces=4,
+        adjacent_faces=[(1,2), (1,3), (1,4), (2,3), (2,4), (3,4)],
+        num_faces_on_vertices=[3],
+        opposing_faces=[(1,3), (2,4)]
+    )
     print('Hello World')
--- graphs.py
+++ graphs.py
@@ -1,11 +1,17 @@
 class Vertex:
     def __init__(self, name):
-        self.name = self.name
+        self.name = name
+
+    def __str__(self):
+        return str(self.name)
 
 class WeightedVertex(Vertex):
     def __init__(self, name, weight):
         super().__init__(name)
         self.weight = weight
+
+    def __str__(self):
+        return "{}|{}".format(self.name, self.weight)
 
 class Edge:
     def __init__(self, src_vert: Vertex, dst_vert: Vertex, directed: bool=False):
@@ -20,6 +26,12 @@ class Edge:
         else:
             return self.src == other.src and self.dst == other.dst or self.src == other.dst and self.dst == other.src
 
+    def __str__(self):
+        if self.directed:
+            return "{}->{}".format(self.src, self.dst)
+        else:
+            return "{}--{}".format(self.src, self.dst)
+
     def follow(self, src_vertex):
         if self.directed:
             assert self.src == src_vertex
@@ -30,6 +42,30 @@ class Edge:
                 return self.dst
             else:
                 return self.src
+
+class UndirectedPath:
+    def __init__(self, verts=[]):
+        self.verts = verts
+
+    def __eq__(self, other):
+        other_set = set(other.verts)
+        self_set = set(self.verts)
+        return len(self_set.difference(other_set)) == 0
+
+    def append(self, other):
+        self.verts.append(other)
+
+    def __str__(self):
+        return str([str(v) for v in self.verts])
+
+    def copy(self):
+        return UndirectedPath(self.verts.copy())
+
+    def __len__(self):
+        return len(self.verts)
+
+    def __getitem__(self, *args, **kwargs):
+        return self.verts.__getitem__(*args, **kwargs)
 
 class Graph:
     def __init__(self, vertices: list[Vertex], edges: list[Edge]):
