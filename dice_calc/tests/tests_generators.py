8aecbb42d890d69d7ba07872cae434897a65c970
matthew-buglass <buglassmatthew@gmail.com>
2023-11-23 08:51:44 -0600

    Added back the d20 tests due to the reduced search space

--- dice_calc/tests/tests_generators.py
+++ dice_calc/tests/tests_generators.py
@@ -26,6 +26,17 @@ class TestFaceWeightGenerator(unittest.TestCase):
 
         self.assertEqual(expected_permutations, received_perms)
 
+    def test_number_of_weights_d20(self):
+        faces = 20
+        opposing_faces = [(1,20), (2,19), (3,18), (4,17), (5,16), (6,15), (7,14), (8,13), (9,12), (10,11)]
+        expected_permutations = 362880 # 9!
+
+        received_perms = 0
+        for _ in face_weights_locked_one(num_faces=faces, opp_faces=opposing_faces):
+            received_perms += 1
+
+        self.assertEqual(expected_permutations, received_perms)
+
     def test_each_number_appears_once_d6(self):
         faces = 6
         opposing_faces = [(0,5), (1,4), (2,3)]
@@ -38,6 +49,14 @@ class TestFaceWeightGenerator(unittest.TestCase):
         faces = 12
         opposing_faces = [(0,11), (1,10), (2,9), (3,8), (4,7), (5,6)]
         expected_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+
+        for weights in face_weights_locked_one(num_faces=faces, opp_faces=opposing_faces):
+            self.assertSetEqual(expected_numbers, set(weights))
+
+    def test_each_number_appears_once_d20(self):
+        faces = 20
+        opposing_faces = [(1, 20), (2, 19), (3, 18), (4, 17), (5, 16), (6, 15), (7, 14), (8, 13), (9, 12), (10, 11)]
+        expected_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 
         for weights in face_weights_locked_one(num_faces=faces, opp_faces=opposing_faces):
             self.assertSetEqual(expected_numbers, set(weights))
@@ -60,16 +79,32 @@ class TestFaceWeightGenerator(unittest.TestCase):
             for i, j in opposing_faces:
                 self.assertEqual(expected_sum, weights[i] + weights[j])
 
-    def test_correct_number_of_elements_d6(self):
+    def test_opposite_sides_add_to_21_d20(self):
+        faces = 20
+        opposing_faces = [(1, 20), (2, 19), (3, 18), (4, 17), (5, 16), (6, 15), (7, 14), (8, 13), (9, 12), (10, 11)]
+        expected_sum = 21
+
+        for weights in face_weights_locked_one(num_faces=faces, opp_faces=opposing_faces):
+            for i, j in opposing_faces:
+                self.assertEqual(expected_sum, weights[i] + weights[j])
+
+    def test_one_weight_per_side_d6(self):
         faces = 6
         opposing_faces = [(0,5), (1,4), (2,3)]
 
         for weights in face_weights_locked_one(num_faces=faces, opp_faces=opposing_faces):
             self.assertEqual(faces, len(weights))
 
-    def test_correct_number_of_elements_d12(self):
+    def test_one_weight_per_side_d12(self):
         faces = 12
         opposing_faces = [(0,11), (1,10), (2,9), (3,8), (4,7), (5,6)]
+
+        for weights in face_weights_locked_one(num_faces=faces, opp_faces=opposing_faces):
+            self.assertEqual(faces, len(weights))
+
+    def test_one_weight_per_side_d20(self):
+        faces = 20
+        opposing_faces = [(1, 20), (2, 19), (3, 18), (4, 17), (5, 16), (6, 15), (7, 14), (8, 13), (9, 12), (10, 11)]
 
         for weights in face_weights_locked_one(num_faces=faces, opp_faces=opposing_faces):
             self.assertEqual(faces, len(weights))
