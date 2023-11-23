8df66f8c838ea9eabe6f1cc3457da568fe7a5fae
matthew-buglass <buglassmatthew@gmail.com>
2023-11-23 09:20:33 -0600

    Added a test file for the graph components

---
+++ dice_calc/tests/tests_graphs.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class TestVertex(unittest.TestCase):
+    def test_vertex_equivalency(self):
+        self.assertEqual(True, False)  # add assertion here
+
+class TestWeightedVertex(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+class TestEdge(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+class TestUndirectedPath(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
